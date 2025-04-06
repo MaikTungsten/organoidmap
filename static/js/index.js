3cdf7891a1fbaa12c84d170849e83449672b8969
Caroline <131356373+caro-cmd@users.noreply.github.com>
2025-04-06 22:54:15 +0200

    Added header

--- static/js/index.js
+++ static/js/index.js
@@ -8,9 +8,109 @@ async function initializePage() {
 
     const large_table = await fetch_large_table();
 
+    //#region Header
+    const header_search_input = document.querySelector("#header-search input");
+    const header_search_btn = document.querySelector("#header-search #search-btn");
+    const select_options = document.querySelector("#header-search .select-options");
+    const select_textfield = document.querySelector("#header-search .select-text-field");
+    const select = document.querySelector("#header-search .select");
+
+    //  add options to select
+    const table_columns = ["All_Fields"].concat(Object.keys(large_table[0])); // get all table columns for the field filter
+
+    let active_opt;
+
+    // loop through the table columns and add options
+    table_columns.forEach(col => {
+        const option = document.createElement("div");
+        option.id = col.replace(/\s+/g, "-");
+        option.ariaLabel = col;
+        option.classList.add("select-option");
+
+        option.innerText = col.toLowerCase()
+                              .split("_")
+                              .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+                             .join(" ");
+
+        select_options.appendChild(option);
+
+        // add function to options
+        option.onclick = function(){
+            select_textfield.innerText = option.innerText;
+            select_textfield.setAttribute("data-value", col);
+
+            // set the active attribute to active option
+            if(active_opt){
+                active_opt.classList.remove("active");
+            }else{
+                option.classList.add("active");
+                active_opt = option;
+            }
+
+            select.click(); // simulate slect click to hide options
+        };
+    });
+
+    // initialize all fields as the active option
+    const all_fields_button = document.getElementById("All_Fields");
+    all_fields_button.click();
+
+    // add functions to the select field
+    let open = false;
+    select.onclick = function() {
+        if (open){
+            select_options.style.display = "none";
+            open = false;
+            select.classList.remove("open");
+        }else{
+            select_options.style.display = "flex";
+            open = true;
+            select.classList.add("open");
+        }
+    };
+
     
-    //#region datatable
     
+    // handle search btn
+    header_search_btn.onclick = function(){
+        button_clicked = true;
+        const value = header_search_input.value;
+        console.log(value)
+        
+    }
+
+    // handle if enter is clicked
+    header_search_input.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            const value = event.target.value;
+            console.log(value);
+        }
+    });
+
+
+    const stats = document.querySelectorAll('.stats-number');
+
+    const papers_number_field = Array.from(stats).find(el => el.id === 'papers-number');
+
+    console.log(papers_number_field)
+
+    papers_number_field.target = large_table.length;
+    papers_number_field.innerText = 600;
+
+    console.log(papers_number_field)
+
+    animateStats();
+
+
+
+
+
+
+    //#endregion Header
+
+    
+    //#region Datatable
+    createDataTable(large_table);
     //#endregion datatable
 
 
@@ -158,6 +258,82 @@ async function initializePage() {
 
 initializePage()
 
+//#region Function for Header
+/**
+ * Function that animates the stat counter
+ */
+
+function animateStats() {
+    const stats = document.querySelectorAll('.stats-number');
+
+    for(let stat of stats){
+        const updateStats = function() {
+
+            const target =+ stat.getAttribute('target');
+            const count =+ stat.innerText;
+            const speed = 1000000000000000;
+
+            const inc = target / speed; 
+            if (count < target){
+                stat.innerText = Math.ceil(count + inc);
+                setTimeout(updateStats, 1);
+            } else {
+                stat.innerText = target;
+            }
+        }
+        updateStats();
+    }
+}
+//#endregion Function for Header
+
+//#region Functions for Section Datatable
+
+/**
+ * Function that creates the table for the dataset and allows filtering
+ * @param {LargeTable[]} large_table - Array of LargeTable.
+ */
+function createDataTable(large_table){
+
+    // create the columns for the table
+    const tableCols = [
+        {title: "Group", field: "group"},
+        {title: "Subgroup", field: "subgroup"},
+        {title: "Title", field: "title", formatter: "textarea", width: 350},
+        // {title: "Journal", field: "journal"},
+        {title: "Year", field: "year"},
+        {title: "DOI", field: "doi", formatter:"link", width: 200},
+        {title: "Model", field: "model"},
+        {title: "Cell Origin", field: "cell_origin"},
+        {title: "Application", field: "application", formatter: "textarea", width: 450},
+        {title: "Advantages", field: "advantages", formatter: "textarea", width: 600},
+        {title: "Limitations", field: "limitations", formatter: "textarea", width: 450}
+    ];
+
+
+
+
+    // create the tabulator table 
+    var table = new Tabulator("#table-container", {
+        data: large_table,
+        columns: tableCols,
+        placeholder: "Nothing available",
+
+        // add pagination
+        pagination:"local",
+        paginationSize: 5,
+        paginationSizeSelector:[5, 10, 25, 50, 100, 250],
+        paginationCounter:"rows",  
+    })
+
+    // download table when button is clicked
+    // const downloadButton = document.getElementById("download-table-button");
+    // downloadButton.addEventListener('click', () => {
+    //     table.download("csv", "data_overview.csv");
+    // });
+}
+//#endregion Functions for Section Datatable
+
+//#region Functions for Section Abbreviation
 /**
  * Function to return all Abbreviations that start with the given letter.
  * @param {*} grouped_abbreviations 
@@ -191,3 +367,5 @@ function createAndAppendAbbreviationCard(abbreviation, parent_container){
 
     parent_container.appendChild(card);
 }
+
+//#endregion Functions for Section Abbreviation
